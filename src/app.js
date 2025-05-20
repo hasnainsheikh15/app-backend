160e032e4bc0bc981f783e3f2a1f01a271150981
hasnainsheikh15 <mdhasnainsheikh15@gmail.com>
2025-05-21 00:04:12 +0530

    utils were written

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+  })
+);
+
+// getting ready to take the essentials in the server using express 
+
+app.use(express.json({limit:"16mb"}));
+app.use(express.urlencoded({extended:true,limit:"16mb"}))
+app.use(express.static("public"))
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import "dotenv/config";
 import  connectDB  from "./db/index.js";
+import app from "./app.js"
+
 
 // dotenv.config({
 //   path: "./env",
 // });
 
-connectDB();
+connectDB()
+.then (() => {
+    app.listen(process.env.PORT || 8000 , () =>{
+        console.log(`Server is listening at port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("Error in DB: ",err);
+    
+})
 
 // const app = express();
 // (async () => {
