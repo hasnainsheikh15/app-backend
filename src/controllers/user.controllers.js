212e30d08fb836b8657ccc612236784fd64c3543
hasnainsheikh15 <mdhasnainsheikh15@gmail.com>
2025-05-22 22:38:37 +0530

    added register controllers and bugs fixed

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,10 +1,86 @@
 import { asyncHandler } from "../utils/asynchandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message:"Okay"
-    })
-}
-)
+  // get user detauils from the frontend
+  // validate the data received in the frontend
+  // check if the user already exixts or not
+  // if not ? create the user : say already existed
+  // if not then take the username password email and fullName from the user
+  // now take the avatar and coverImage and uppload it to the cloudinary
+  // create the user object and save it to the database as it is nosql database
+  // remove the password and refrsh token from the response
+  // return the response ...
+
+  const { fullName, username, email, password } = req.body; // taking the data from the postman as a frontend..
+  //  console.log(username);
+  const regEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const emailToValidate = email.trim().toLowerCase();
+  if (
+    // this is basically an js inbuild function which checks if the fields in the array is present and if it is present and then it will trim it and if the output after trim is empty then it will return true and if the fields are present then it will return false.And this function will traverse on all the fiels in the array and work like an operator
+    [fullName, username, email, password].some(
+      (fields) => fields?.trim() === ""
+    )
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  if (!regEx.test(emailToValidate)) {
+    throw new ApiError(400, "Email is not valid");
+  }
+
+  // check if the user is present or not
+
+  const userExisted = User.findOne({
+    $or: [{ username }, { email }], // this is an or operator which will check if the username or email is present in the database , commonly called as filterQuery in mogoDb and this does not work in plain js .
+  });
+
+  // final condition for the user to be present or not
+  if (userExisted) throw new ApiError(409, "User already exists");
+
+  // now for the avatar and coverImages
+
+  const avatarLocalPath = req.files?.avatar[0]?.path; // this is because i have used the multer middleware and this is the path where the image is stored in the local storage
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  // now check if the avatar is given or not
+
+  if (!avatarLocalPath) throw new ApiError(400, "Avatar is required");
+
+  // now uploading on the cloudinary
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  // again check if the avatar is uploaded or not
+  if (!avatar) throw new ApiError(500, "Avatar is not uploaded");
+
+  // now create the user object and save it to the database
+  const user = await User.create({
+    fullName,
+    username: username.toLowerCase(),
+    email: emailToValidate,
+    password,
+    avatar: avatar?.url,
+    coverImage: coverImage?.url || "",
+  });
+
+  const userCreated = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!userCreated)
+    throw new ApiError(
+      500,
+      "Something went wrong while registering the User..."
+    );
+
+    // send the response to the client about the registeration
+    return res.status(201).json(
+        new ApiResponse(201,userCreated,"User registered Successfully")
+    )
+});
 
 export { registerUser };
